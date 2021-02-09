d0db856f8c0a8f3b668efa81c43946bbf36d1d3e
Teodor Georgiev <tageogriev@gmail.com>
2021-02-09 13:35:41 +0200

    init commit

---
+++ src/codexio/Problem3_1_evenOrOdd.java
@@ -0,0 +1,12 @@
+package codexio;
+
+/**
+ * 1.Напишете израз, който да проверява дали дадено цяло число е четно или нечетно
+ */
+public class Problem3_1_evenOrOdd {
+    public static void main(String[]args){
+       int a = 3;
+       int b = a%2;
+       System.out.println(b==0?"even":"odd");
+    }
+}
