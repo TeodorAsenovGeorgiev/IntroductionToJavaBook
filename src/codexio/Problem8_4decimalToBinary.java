4f9a18e89c15025c37d07141d98fcf35c55de20e
Teodor Asenov Georgiev <tageorgiev@gmail.com>
2021-02-26 01:11:30 +0200

    Problems8_4,5

--- src/codexio/Problem8_4decimalToBinary.java
+++ src/codexio/Problem8_4decimalToBinary.java
@@ -6,9 +6,21 @@ public class Problem8_4decimalToBinary {
     public static void main(String [] args){
         Scanner input = new Scanner(System.in);
         System.out.println("Въведете число: ");
-        double decimal = input.nextDouble();
+        int decimal = input.nextInt();
+        int[] reverseBinary = new int[64];
+        int index = 0;
 
-        System.out.println(decimal/2);
+      while (decimal>0){
+        reverseBinary[index++]=decimal%2;
+        decimal=decimal/2;
+      }
+        for (int i = index-1; i >=0 ; i--) {
+            System.out.print(reverseBinary[i]);
+        }
+
+        }
 
     }
-}
+
+
+
---
+++ src/codexio/Problem8_binaryToDecimal.java
@@ -0,0 +1,25 @@
+package codexio;
+
+import java.util.Scanner;
+
+public class Problem8_binaryToDecimal {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Въведете число: ");
+        String inputString= input.nextLine();
+      int [] binary = new int[inputString.length()];
+        for (int i = 0; i <inputString.length(); i++) {
+            binary[i]=Integer.parseInt(String.valueOf(inputString.charAt(i)));
+        }
+        int result = 0;
+        int power = binary.length-1;
+        for (int i = 0; i <=binary.length-1 ; i++) {
+            result+= binary[i]* Math.pow(2,power);
+            power--;
+        }
+
+        System.out.println(result);
+    }
+
+
+}
